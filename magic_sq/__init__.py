7445119652f2da4160f42b61eacde5ba38e0709c
Ryan Heard <ryanwheard@gmail.com>
2025-08-16 10:41:32 -0400

    after testing, this seems to be the fastest way to do things... Check the right column first then calculate e

--- magic_sq/__init__.py
+++ magic_sq/__init__.py
@@ -16,6 +16,7 @@ def test(i, show_puzzles=False, limited_checks=False):
         #   and then fit them to the parameterization based on size of the parameters.
         #   I haven't fully convinced myself thats something I can do tho...
         out_d, out_f = d1, d2
+        out_f_sq = out_f**2
         a1_sq, a2_sq = a1**2, a2**2
         b1_sq, b2_sq = b1**2, b2**2
         c1_sq, c2_sq = c1**2, c2**2
@@ -27,22 +28,33 @@ def test(i, show_puzzles=False, limited_checks=False):
                     ((a1, a1_sq), (b2, b2_sq), (c1, c1_sq), (a2, a2_sq), (b1, b1_sq), (c2, c2_sq)),
                     ((a1, a1_sq), (b1, b1_sq), (c2, c2_sq), (a2, a2_sq), (b2, b2_sq), (c1, c1_sq)),
                 ):
-            # We're going to compute the central value from the top row
-            target_val = out_a_sq + out_b_sq + out_c_sq
+            target_val = out_a_sq + out_b**2 + out_c_sq
             if target_val <= i:
                 continue  # out_e is less than 1 (not valid)
+            out_i_sq = out_i**2
+            if out_c_sq + out_f_sq + out_i_sq != target_val:
+                continue  # right column doesn't equal top row
+
+            # Nothing past here runs:
             if show_puzzles:
                 print(f'{out_a:<16}**2 {out_b:<16}**2 {out_c:<16}**2')
-                print(f'{out_d:<16}**2 0**2 {out_f:<16}**2')
+                print(f'{out_d:<16}**2 {0:<16}**2 {out_f:<16}**2')
                 print(f'{out_g:<16}**2 {out_h:<16}**2 {out_i:<16}**2')
                 print()
-                show_puzzles = False
-            if out_g_sq + out_h_sq + out_i_sq != target_val:
-                continue
+
+            # region Calculate e
             out_e_sq = target_val - i  # central value, squared
             out_e = math.isqrt(out_e_sq)
             if out_e**2 != out_e_sq:
                 continue  # e is not a perfect square
+            # endregion
+
+            out_g_sq = out_g**2
+            if out_g_sq + out_h_sq + out_i_sq != target_val:
+                continue  # Bottom row doesn't equal top row
+            if out_a_sq + out_d**2 + out_g_sq != target_val:
+                continue  # left column doesn't equal top row
+            # TODO: Check diagonols too
             ans = f'{out_a:<16}**2 {out_b:<16}**2 {out_c:<16}**2\n' \
                   f'{out_d:<16}**2 {out_e:<16}**2 {out_f:<16}**2\n' \
                   f'{out_g:<16}**2 {out_h:<16}**2 {out_i:<16}**2\n\n'
